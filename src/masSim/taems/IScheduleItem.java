84dc509c1605ed2c8686a41937977afacfd19583
Edwin Vargas <ev16@txstate.edu>
2013-02-26 17:32:59 -0600

    Removed ../Taems/*ScheduleItem.java, as the use of ScheduleElement was resumed

--- src/masSim/taems/IScheduleItem.java
+++
@@ -1,9 +0,0 @@
-package masSim.taems;
-
-import masSim.taems.ScheduleItem.Status;
-
-public interface IScheduleItem {
-	  public String getName();
-	  public Status update(int dt);
-	  public Status getStatus();
-}
